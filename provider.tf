21d455703d9d55554c3cd120550f898f4c469205
Snekhapugal <SnekhaPuhazendhi>
2025-06-30 17:46:24 +0530

    latestproviderconfig

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.13"
+  required_version = ">= 1.5.0"
 
   required_providers {
     google = {
-      version = "4.1.0"
+      version = "6.41.0"
       source  = "hashicorp/google"
     }
   }
